6c0b4bda6a50ad130d9cd19f3223df75fafe3028
damasofc <damasofc@hotmail.com>
2017-05-26 15:45:11 -0600

    ya esta merge y aca esta funcionando en todo

---
+++ app/src/main/java/com/example/jhair/proyecto/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.jhair.proyecto;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button entrar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initComponents();
+    }
+    private void initComponents(){
+        entrar = (Button)findViewById(R.id.button2);
+        entrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,MenuMainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
